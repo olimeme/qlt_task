88b4e2c6e4a0ab2c1a8415d68313fd6a14b33687
Alibek <alibek.mombekov@mail.ru>
2021-09-29 23:12:48 +0600

    retouchings

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Dec 29, 2021 00:00:00").getTime();
+var countDownDate = new Date("Nov 30, 2021 00:00:00").getTime();
 
 var x = setInterval(function() {
     var now = new Date().getTime();
