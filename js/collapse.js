ec228d216d894d9100afcaf37914d4068f670d65
Alibek <alibek.mombekov@mail.ru>
2021-09-30 18:19:47 +0600

    arrows added to collapse

--- js/collapse.js
+++ js/collapse.js
@@ -3,6 +3,16 @@ const coll = document.querySelectorAll(".collapse_btn");
 for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     let cont = this.nextElementSibling;
+    let arrow = this.children[0];
     cont.classList.toggle("unactive_collapse");
+    if(arrow.classList.contains("up")){
+      arrow.classList.remove("up");
+      arrow.classList.add("down");
+    }
+    else {
+      arrow.classList.remove("down");
+      arrow.classList.add("up");
+    }
 });
 }
+
